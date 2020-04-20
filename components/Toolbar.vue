1f31093c5ce0c96189996c5b6f0605fcbbebcbb9
Daijiro Igeta <lemon95tennis@gmail.com>
2020-04-20 14:24:29 +0900

    indexページの作成

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -5,7 +5,7 @@
         <nuxt-link to="/" class="toolbar-title">天気検索アプリ</nuxt-link>
       </v-toolbar-title>
       <v-btn text>
-        <nuxt-link to="/" class="toolbar-title">場所から検索</nuxt-link>
+        <nuxt-link to="/place" class="toolbar-title">場所から検索</nuxt-link>
       </v-btn>
       <v-btn text>
         <nuxt-link to="/map" class="toolbar-title">地図から検索</nuxt-link>
@@ -14,7 +14,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .toolbar-title {
   color: #ffffff !important;
   text-decoration: none;
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,50 @@
 <template>
   <v-layout justify-center align-center>
     <v-flex>
-      <div class="text-center">
-        <vuetify-logo />
-      </div>
-      <search-form />
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">天気検索アプリ</h2>
+
+        <v-responsive
+          class="mx-auto title font-weight-light text-left mb-8"
+          max-width="720"
+        >
+          このWEBアプリは天気の検索ができるアプリです。天気の検索方法には二通りあり、調べたい場所を入力する方法と、地図から選ぶ方法があります。
+        </v-responsive>
+
+        <v-avatar class="elevation-12 mb-12" size="128">
+          <v-img :src="require('@/assets/images/sunny.png')"></v-img>
+        </v-avatar>
+
+        <div>
+          <v-btn color="primary" large>
+            <span class="font-weight-bold">
+              <nuxt-link to="/place" class="link-button"
+                >場所から検索</nuxt-link
+              >
+            </span>
+          </v-btn>
+          <v-btn color="secondary" large>
+            <span class="font-weight-bold">
+              <nuxt-link to="/map" class="link-button">地図から検索</nuxt-link>
+            </span>
+          </v-btn>
+        </div>
+      </v-container>
+
+      <div class="py-12"></div>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-import SearchForm from '~/components/SearchForm.vue'
-
 export default {
-  components: {
-    VuetifyLogo,
-    SearchForm
-  }
+  components: {}
 }
 </script>
+
+<style lang="scss" scoped>
+.link-button {
+  color: #ffffff !important;
+  text-decoration: none;
+}
+</style>
