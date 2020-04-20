59b8be58f6368cbdde93b27a6e63860e3967b98c
井桁第二朗 <igeta@life2bits.com>
2020-04-21 00:37:21 +0900

    deploy設定の変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
-const routerBase =
-  process.env.DEPLOY_ENV === 'GH_PAGES'
-    ? {
-        router: {
-          base: '/weather-app/'
-        }
-      }
-    : {}
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/weather-app/'
+  }
+} : {}
 
 export default {
   mode: 'spa',
