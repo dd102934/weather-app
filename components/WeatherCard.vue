fede04a2c3dbd8e237470f9355081f77b09f88c3
dd102934 <lemon95tennis@gmail.com>
2020-05-16 16:09:08 +0900

    天気を表示するコンポーネントを追加

---
+++ components/WeatherCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="map-height">
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title class="pb-0"
+        >場所：{{ forecastData.location }}</v-card-title
+      >
+      <v-img
+        class="white--text align-end"
+        max-height="230px"
+        contain
+        :src="require(`@/assets/images/${forecastData.weather}.png`)"
+      >
+      </v-img>
+      <v-card-text class="text--primary">
+        <p class="text-left mb-0">国：{{ forecastData.country }}</p>
+        <p class="text-left mb-0">{{ forecastData.summary }}</p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forecastData: Object
+  }
+}
+</script>
+<style lang="scss" scoped>
+.map-height {
+  height: 60vh;
+  p {
+    margin-bottom: 0px;
+    text-align: left;
+  }
+}
+</style>
